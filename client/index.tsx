7eca677fb8c09255d1d8206c5702d5226bfb699f
Hammad <hammad@celtx.com>
2022-02-26 19:03:13 -0330

    Setup basic project structure and add a hello world page to the main screen

---
+++ client/index.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { HomePage } from "./src/HomePage";
+
+ReactDOM.render(<HomePage />, document.getElementById('mh-main-body'));
---
+++ client/src/HomePage.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+
+export const HomePage = (): JSX.Element => {
+    return (
+        <div>Hello World</div>
+    )
+}
